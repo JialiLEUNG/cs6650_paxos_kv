1e229807260434e5c834bbc0f93f612cb6f706c2
jialiLEUNG <liang.jiali.cs@gmail.com>
2020-04-17 13:42:45 -0700

    modify paxoscallback by adding in readwritelock

--- src/main/java/com/github/jiali/paxos/nodes/PaxosCallbackImpl.java
+++ src/main/java/com/github/jiali/paxos/nodes/PaxosCallbackImpl.java
@@ -11,6 +11,7 @@ public class PaxosCallbackImpl implements PaxosCallback {
      */
     private ConcurrentMap<String, String> kvStore = new ConcurrentHashMap<>();
     private Gson gson = new Gson();
+    ReadWriteLock rwl = new ReadWriteLock();
 
     @Override
     public void callback(byte[] msg) {
@@ -19,21 +20,74 @@ public class PaxosCallbackImpl implements PaxosCallback {
          */
         String msgString = new String(msg);
         // convert JSON string to Java objects
-        ClientInput bean = gson.fromJson(msgString, ClientInput.class);
-        switch (bean.getOperation()) {
+        ClientInput clientInput = gson.fromJson(msgString, ClientInput.class);
+        switch (clientInput.getOperation()) {
             case "get":
-                System.out.println("GET KEY: " + bean.getKey() + "Result: " + kvStore.get(bean.getKey()) + " Succeed");
+                get(clientInput);
                 break;
             case "put":
-                kvStore.put(bean.getKey(), bean.getValue());
-                System.out.println("PUT KEY: " + bean.getKey() + " VALUE: " + bean.getValue() + " Succeed.");
+                put(clientInput);
                 break;
             case "delete":
-                kvStore.remove(bean.getKey());
-                System.out.println("DELETE KEY: " + bean.getKey() + " Succeed.");
+                delete(clientInput);
                 break;
             default:
                 break;
         }
     }
+
+    private void get(ClientInput clientInput) {
+
+        try{
+            rwl.lockRead();
+
+            String value = kvStore.get(clientInput.getKey());
+
+            if (value != null){
+                // we get as many value objects as we need to return to the client
+                // (in this case, we select them from the service’s value collection
+                // based on whether they’re inside our request getRequest),
+                // and write them each in turn to the response observer using its onNext() method.
+
+                System.out.println("GET KEY: " + clientInput.getKey() + "Result: " + value + " Succeed");
+            } else{
+                System.out.println("----- Fail: Key does not exist. GET fail.");
+            }
+            rwl.unlockRead();
+        } catch (InterruptedException e) {
+        }
+    }
+
+    private void put(ClientInput clientInput){
+        try{
+            rwl.lockWrite();
+
+            String result = kvStore.putIfAbsent(clientInput.getKey(), clientInput.getValue());
+
+            if(result == null){
+                System.out.println("PUT KEY: " + clientInput.getKey() + " VALUE: " + clientInput.getValue() + " Succeed.");
+            } else{
+                System.out.println("----- Fail: Key already exists. PUT fail.");
+            }
+            rwl.unlockWrite();
+        } catch (Exception e){
+        }
+    }
+
+    private void delete(ClientInput clientInput){
+        try{
+            rwl.lockWrite();
+
+            String result = kvStore.remove(clientInput.getKey());
+            if (result != null){
+                System.out.println("DELETE KEY: " + clientInput.getKey() + " Succeed.");
+            } else{
+                System.out.println("----- Fail: Key does not exist. DELETE fail");
+            }
+            rwl.unlockWrite();
+        }catch (Exception e){
+        }
+    }
+
+
 }
---
+++ src/main/java/com/github/jiali/paxos/nodes/ReadWriteLock.java
@@ -0,0 +1,33 @@
+package com.github.jiali.paxos.nodes;
+
+public class ReadWriteLock {
+    private int readers = 0;
+    private int writers = 0;
+    private int writeRequests = 0;
+
+    public synchronized void lockRead() throws InterruptedException{
+        while(writers > 0 || writeRequests < 0){
+            wait();
+        }
+        readers++;
+    }
+
+    public synchronized void unlockRead(){
+        readers--;
+        notifyAll(); // Wakes up all threads that are waiting
+    }
+
+    public synchronized void lockWrite() throws InterruptedException{
+        writeRequests++;
+        while(readers > 0 || writers > 0){
+            wait();
+        }
+        writeRequests--;
+        writers++;
+    }
+
+    public synchronized void unlockWrite() throws InterruptedException{
+        writers--;
+        notifyAll(); // Wakes up all threads that are waiting
+    }
+}
